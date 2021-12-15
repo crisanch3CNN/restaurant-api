b8e96d03ff1ea0e89cac5c50dc1be975fb9320e5
crisanch <cristian.sanchez@triciclos.cl>
2021-12-15 16:37:33 -0300

    feat(api): add login and user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Las credenciales no coinciden con los registros en nuestra base de datos']
+            ]);
+        }
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+}
