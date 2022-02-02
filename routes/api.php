ef4b6d04408464810b804f4aff7601935d419a6e
crisanch <cristian.sanchez@triciclos.cl>
2022-02-02 19:46:42 -0300

    feat(route/api): add routes api resources

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
